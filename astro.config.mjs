272bd800508c81287ab83baf2dbbd52013a1fb12
Douglas Lopes <dl.aguiar@yahoo.com.br>
2025-09-22 17:48:47 -0300

    feat: add site URL to astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import alpinejs from '@astrojs/alpinejs';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://vmmadeiras.com.br',
   vite: {
     plugins: [tailwindcss()]
   },
